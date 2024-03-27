477d0d04eb08e0d77bc4e72a9afa9e9c3c6ebd12
Rafael Alves <alvesrafa.dev@gmail.com>
2024-03-27 18:03:11 -0300

    job service, starting rabbit and job workers

--- encoder/application/services/job_service.go
+++ encoder/application/services/job_service.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"errors"
 	"os"
+	"strconv"
 
 	"github.com/alvesrafa/video-encoder/application/repositories"
 	"github.com/alvesrafa/video-encoder/domain"
@@ -15,14 +17,78 @@ type JobService struct {
 
 func (j *JobService) Start() error {
 	err := j.ChangeJobStatus("DOWNLOADING")
-
 	if err != nil {
 		return j.failJob(err)
 	}
 	err = j.VideoService.Download(os.Getenv("inputBucketName"))
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.ChangeJobStatus("FRAGMENTING")
+	if err != nil {
+		return j.failJob(err)
+	}
+	err = j.VideoService.Fragment()
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.ChangeJobStatus("ENCODING")
+	if err != nil {
+		return j.failJob(err)
+	}
+	err = j.VideoService.Encode()
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.performUpload()
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.ChangeJobStatus("FINISHING")
+	if err != nil {
+		return j.failJob(err)
+	}
+	err = j.VideoService.Finish()
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	err = j.ChangeJobStatus("COMPLETED")
+	if err != nil {
+		return j.failJob(err)
+	}
 
 	return nil
 }
+
+func (j *JobService) performUpload() error {
+	err := j.ChangeJobStatus("UPLOADING")
+
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	videoUpload := NewVideoUpload()
+	videoUpload.OutputBucket = os.Getenv("outputBucketName")
+	videoUpload.VideoPath = os.Getenv("localStoragePath") + "/" + j.VideoService.Video.ID
+	concurrency, _ := strconv.Atoi(os.Getenv("CONCURRENCY_UPLOAD"))
+	doneUpload := make(chan string)
+
+	go videoUpload.ProcessUpload(concurrency, doneUpload)
+
+	uploadResult := <-doneUpload
+
+	if uploadResult != "upload completed" {
+		return j.failJob(errors.New(uploadResult))
+	}
+
+	return err
+}
+
 func (j *JobService) ChangeJobStatus(status string) error {
 	var err error
 	j.Job.Status = status
