90110f22fd95c5eb8ec1b6d606392bd213eaed67
Rafael Alves <alvesrafa.dev@gmail.com>
2024-03-28 16:54:55 -0300

    job manager

--- encoder/application/services/job_manager.go
+++ encoder/application/services/job_manager.go
@@ -1,1 +1,131 @@
 package services
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/alvesrafa/video-encoder/application/repositories"
+	"github.com/alvesrafa/video-encoder/domain"
+	"github.com/alvesrafa/video-encoder/framework/queue"
+	"github.com/jinzhu/gorm"
+	"github.com/streadway/amqp"
+)
+
+type JobManager struct {
+	Db               *gorm.DB
+	Domain           domain.Job
+	MessageChannel   chan amqp.Delivery
+	JobReturnChannel chan JobWorkerResult
+	RabbitMQ         *queue.RabbitMQ
+}
+
+type JobNotificationError struct {
+	Message string `json:"message"`
+	Error   string `json:"error"`
+}
+
+func NewJobManager(db *gorm.DB, rabbitMQ *queue.RabbitMQ, jobReturnChannel chan JobWorkerResult, messageChannel chan amqp.Delivery) *JobManager {
+	return &JobManager{
+		Db:               db,
+		Domain:           domain.Job{},
+		MessageChannel:   messageChannel,
+		JobReturnChannel: jobReturnChannel,
+		RabbitMQ:         rabbitMQ,
+	}
+}
+
+func (j *JobManager) Start(ch *amqp.Channel) {
+	videoService := NewVideoService()
+	videoService.VideoRepository = repositories.VideoRepositoryDb{Db: j.Db}
+
+	jobService := JobService{
+		JobRepository: repositories.JobRepositoryDb{Db: j.Db},
+		VideoService:  videoService,
+	}
+
+	concurrency, err := strconv.Atoi(os.Getenv("CONCURRENCY_WORKERS"))
+
+	if err != nil {
+		log.Fatalf("error loading var: CONCURRENCY_WORKERS \n %v", err)
+		concurrency = 1
+	}
+
+	for qtdProcesses := 0; qtdProcesses < concurrency; qtdProcesses++ {
+		go JobWorker(j.MessageChannel, j.JobReturnChannel, jobService, qtdProcesses, j.Domain)
+	}
+
+	for jobResult := range j.JobReturnChannel {
+		if jobResult.Error != nil {
+			err = j.checkParseErrors(jobResult)
+		} else {
+			err = j.notifySuccess(jobResult)
+		}
+
+		if err != nil {
+			jobResult.Message.Reject(false)
+		}
+	}
+}
+
+func (j *JobManager) notifySuccess(jobResult JobWorkerResult) error {
+	// Mutex.Lock()
+	jobJson, err := json.Marshal(jobResult.Job)
+	// Mutex.Unlock()
+	if err != nil {
+		return err
+	}
+
+	err = j.notify(jobJson)
+	if err != nil {
+		return err
+	}
+
+	err = jobResult.Message.Ack(false) // acknowledge - informa o rabbitmq q a mensagem esta ok
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func (j *JobManager) checkParseErrors(jobResult JobWorkerResult) error {
+	if jobResult.Job.ID != "" {
+		log.Printf("MessageID %v rejected. Error parsing job #{jobResult.Job.ID} Error: %v", jobResult.Message.DeliveryTag, jobResult.Error.Error())
+
+	} else {
+		log.Printf("MessageID %v rejected. Error: %v", jobResult.Message.DeliveryTag, jobResult.Error.Error())
+	}
+
+	errorMsg := JobNotificationError{
+		Message: string(jobResult.Message.Body),
+		Error:   jobResult.Error.Error(),
+	}
+
+	jobJson, err := json.Marshal(errorMsg)
+	if err != nil {
+		return err
+	}
+
+	err = j.notify(jobJson)
+	if err != nil {
+		return err
+	}
+
+	err = jobResult.Message.Reject(false) // reject message to queue
+
+	return err
+}
+
+func (j *JobManager) notify(jobJson []byte) error {
+
+	err := j.RabbitMQ.Notify(
+		string(jobJson),
+		"application/json",
+		os.Getenv("RABBITMQ_NOTIFICATION_EX"),
+		os.Getenv("RABBITMQ_NOTIFICATION_ROUTING_KEY"),
+	)
+
+	return err
+}
